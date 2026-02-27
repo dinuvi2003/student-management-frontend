062ccee39c409a7615348255e82ad815af89db1c
dinuvi2003 <dinuvi2003@gmail.com>
2026-02-27 11:32:46 +0530

    Small fix in UI

--- src/componenets/layout/TopBar.tsx
+++
@@ -1,9 +0,0 @@
-const Topbar = () => {
-  return (
-    <div className="h-16 bg-white border-b flex items-center px-6">
-      <div className="text-gray-500">☰</div>
-    </div>
-  );
-};
-
-export default Topbar;
